bd3a3c98b41b6bc1f16cfa84930e29d81fb040f7
AlexSchiffke <alexander.schiffke@t-online.de>
2015-05-07 11:19:58 +0200

    Menubar hinzufefügt

--- ImageEditor.java
+++ ImageEditor.java
@@ -1,3 +1,5 @@
+package ImageEditor;
+
 import javax.swing.*; 
 import java.awt.event.*; 
 import java.awt.*; 
@@ -5,10 +7,32 @@ import java.awt.image.*;
 import java.io.*; 
 import javax.imageio.*;  
 
-public class ImageEditorFrame extends JFrame{    
+public class ImageEditorFrame extends JFrame{ 
+  
 	public ImageEditorFrame(){       
 		setDefaultCloseOperation(EXIT_ON_CLOSE);       
 		setSize(400,  300);       
 		setVisible(true);    
 	} 
+	
+	private void createMenuBar(){
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		JMenu menuFile = new JMenu("File");
+		menuBar.add(menuFile);
+		JMenuItem menuItemOpen = new JMenuItem("Open");
+		menuFile.add(menuItemOpen);
+		menuItemOpen.addActionListener(
+			new ActionListener() {
+		         public void actionPerformed(ActionEvent e){
+		        	 onOpen();
+		        	 
+		         }
+			}
+		);
+	}
+	private void onOpen(){
+		JOptionPane.showMessageDialog(this, "Open Selected");
+	}  
+	
 } 
